0f900dec24bba37f1152f67bddc6016fa802bfb9
rafael <rafaeldutraribas@gmail.com>
2025-07-31 21:37:16 -0300

    Move Method frequent_renter_points to rental class

--- the_starting_point/after/custumer.rb
+++ the_starting_point/after/custumer.rb
@@ -19,14 +19,11 @@ class Customer
     frequent_renter_points = 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = element.charge
-      # add frequent renter points
-      frequent_renter_points += 1
-      # add bonus for a two day new release rental
-      frequent_renter_points += 1 if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+      frequent_renter_points += element.frequent_renter_points
+
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+      total_amount += element.charge
     end
     # add footer lines
     result += "Amount owed is #{total_amount}\n"
