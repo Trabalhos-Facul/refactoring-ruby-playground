0b6efafdf53936ae63fcb110efaa077e44109ca3
rafael <rafaeldutraribas@gmail.com>
2025-07-31 22:43:20 -0300

    Replace Type Code with State/Strategy Pattern

--- the_starting_point/after/movie.rb
+++ the_starting_point/after/movie.rb
@@ -1,31 +1,32 @@
+require_relative './regular_price'
+require_relative './new_release_price'
+require_relative './children_price'
+
 class Movie
   REGULAR = 0
   NEW_RELEASE = 1
   CHILDRENS = 2
-  attr_reader :title
-  attr_accessor :price_code
+  attr_reader :title, :price_code
 
   def initialize(title, price_code)
     @title = title
-    @price_code = price_code
+    self.price_code = price_code
+  end
+
+  def price_code=(value)
+    @price_code = value
+    @price = case @price_code
+             when REGULAR then RegularPrice.new
+             when NEW_RELEASE then NewReleasePrice.new
+             when CHILDRENS then ChildrensPrice.new
+             end
   end
 
   def charge(days_rented)
-    result = 0
-    case @price_code
-    when Movie::REGULAR
-      result += 2
-      result += (days_rented - 2) * 1.5 if days_rented > 2
-    when Movie::NEW_RELEASE
-      result += days_rented * 3
-    when Movie::CHILDRENS
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
-    end
-    result
+    @price.charge(days_rented)
   end
 
   def frequent_renter_points(days_rented)
-    @price_code == Movie::NEW_RELEASE && days_rented > 1 ? 2 : 1
+    @price.frequent_renter_points(days_rented)
   end
 end
